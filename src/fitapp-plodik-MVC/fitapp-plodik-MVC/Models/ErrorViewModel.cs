714a13bafbc38e1ae08c2ac540c4dd4f93b75d86
Marek Plodík <plodikmarek@sssvt.cz>
2026-01-05 11:02:20 +0100

    prvni h

---
+++ src/fitapp-plodik-MVC/fitapp-plodik-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fitapp_plodik_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
