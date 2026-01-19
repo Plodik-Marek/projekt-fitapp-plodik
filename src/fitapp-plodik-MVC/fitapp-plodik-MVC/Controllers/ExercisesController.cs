cf75816f6f916a9bdee8964d24b52f64759c35dd
Marek Plodík <plodikmarek@sssvt.cz>
2026-01-19 11:41:08 +0100

    Hotový design

--- src/fitapp-plodik-MVC/fitapp-plodik-MVC/Controllers/ExercisesController.cs
+++ src/fitapp-plodik-MVC/fitapp-plodik-MVC/Controllers/ExercisesController.cs
@@ -3,6 +3,7 @@ using fitapp_plodik_MVC.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace fitapp_plodik_MVC.Controllers
 {
@@ -44,12 +45,30 @@ namespace fitapp_plodik_MVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Exercise exercise)
+        public async Task<IActionResult> Create(Exercise exercise, IFormFile? imageFile)
         {
             if (!ModelState.IsValid)
             {
                 ViewBag.MachineId = new SelectList(_db.Machines, "Id", "Name", exercise.MachineId);
                 return View(exercise);
+            }
+
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/icons/exercises");
+
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var filePath = Path.Combine(folderPath, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+
+                exercise.ImageUrl = "/img/icons/exercises/" + fileName;
             }
 
             _db.Exercises.Add(exercise);
@@ -69,7 +88,7 @@ namespace fitapp_plodik_MVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Exercise exercise)
+        public async Task<IActionResult> Edit(int id, Exercise exercise, IFormFile? imageFile)
         {
             if (id != exercise.Id)
                 return NotFound();
@@ -80,7 +99,40 @@ namespace fitapp_plodik_MVC.Controllers
                 return View(exercise);
             }
 
-            _db.Exercises.Update(exercise);
+            var dbExercise = await _db.Exercises.FirstOrDefaultAsync(e => e.Id == id);
+            if (dbExercise == null)
+                return NotFound();
+
+            dbExercise.Name = exercise.Name;
+            dbExercise.MuscleGroup = exercise.MuscleGroup;
+            dbExercise.Description = exercise.Description;
+            dbExercise.MachineId = exercise.MachineId;
+
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                if (!string.IsNullOrWhiteSpace(dbExercise.ImageUrl) && dbExercise.ImageUrl.StartsWith("/img/"))
+                {
+                    var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", dbExercise.ImageUrl.TrimStart('/').Replace("/", Path.DirectorySeparatorChar.ToString()));
+                    if (System.IO.File.Exists(oldPath))
+                        System.IO.File.Delete(oldPath);
+                }
+
+                var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/icons/exercises");
+
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var filePath = Path.Combine(folderPath, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+
+                dbExercise.ImageUrl = "/img/icons/exercises/" + fileName;
+            }
+
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -104,6 +156,13 @@ namespace fitapp_plodik_MVC.Controllers
             var exercise = await _db.Exercises.FindAsync(id);
             if (exercise == null)
                 return NotFound();
+
+            if (!string.IsNullOrWhiteSpace(exercise.ImageUrl) && exercise.ImageUrl.StartsWith("/img/"))
+            {
+                var imgPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", exercise.ImageUrl.TrimStart('/').Replace("/", Path.DirectorySeparatorChar.ToString()));
+                if (System.IO.File.Exists(imgPath))
+                    System.IO.File.Delete(imgPath);
+            }
 
             _db.Exercises.Remove(exercise);
             await _db.SaveChangesAsync();
